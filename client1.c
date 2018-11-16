a50d1c445e2c79f641e2e18f39a028349140fea6
Amuzing <10791383+Amuzing@users.noreply.github.com>
2018-11-16 15:35:57 +0300

    Update client1.c
    connection and receive of welcome_message

--- client1.c
+++ client1.c
@@ -1,51 +1,101 @@
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+#include <netdb.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+#define PORT "5678"
+#define MAX_SIZE 256
 
-#define HOST_ADDR "www.example.net"
+bool some_func(int fd);
 
-int main() {
-  int status;
-  struct addrinfo hints;
-  struct addrinfo* res;
-  struct addrinfo* p;
-  char ipstr[INET6_ADDRSTRLEN];
-  char my_name[200];
+int main(int argc, char* argv[]) {
+  int fd;
+  struct addrinfo hints, *ai, *p;
+  int nbytes;
+  int yes = 1;
+  int idle_time = 60;
+  int rv;
+  bool is_true = true;
+  char* address = NULL;
+
+  char buf[MAX_SIZE];
 
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-  //hints.ai_flags = AI_PASSIVE;
+  hints.ai_flags = AI_PASSIVE;
 
-  if((status = getaddrinfo(HOST_ADDR, NULL, &hints, &res) != 0)) {
-    fprintf(stderr, "lala %s\n", gai_strerror(status));
+  if (argc == 2) {
+    address = malloc(sizeof(char) * strlen(argv[1]));
+    strcpy(address, argv[1]);
+    memset(&hints.ai_flags, 0, sizeof(hints.ai_flags));
+  }
+
+  if (address != NULL) {
+    printf("connected to %s\n", address);
+  }
+
+  if ((rv = getaddrinfo(address, PORT, &hints, &ai)) != 0) {
+    fprintf(stderr, "CLIENT ERROR: %s\n", gai_strerror(rv));
+    free(address);
+    return 1;
+  }
+  free(address);
+
+  for (p = ai; p != NULL; p = p->ai_next) {
+    if ((fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("socket");
+      continue;
+    }
+
+    if (connect(fd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(fd);
+      perror("connect");
+      continue;
+    }
+
+    break;
+  }
+
+  if (p == NULL) {
+    fprintf(stderr, "cliend: failed to connect\n");
     return 2;
   }
-  printf("IP for %s \n", HOST_ADDR);
 
-  for(p = res; p != NULL; p = p->ai_next) {
-    void* addr;
-    char* ipver;
-    if(p->ai_family == AF_INET) {
-      struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
-      addr = &(ipv4->sin_addr);
-      ipver = "IPv4";
-    } else {
-      struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
-      addr = &(ipv6->sin6_addr);
-      ipver = "IPv6";
-    }
-    inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
-    printf(" %s: %s\n", ipver, ipstr);
+  freeaddrinfo(ai);
+
+  // keep-alive
+  if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &yes, sizeof(yes)) == -1) {
+    perror("setsockopt(SO_KEEPALIVE)");
   }
-  gethostname(my_name, 200);
-  printf("%s\n", my_name);
-  freeaddrinfo(res);
+  if (setsockopt(fd, IPPROTO_IP, TCP_KEEPIDLE, &idle_time, sizeof(idle_time)) == -1) {
+    perror("setsockopt(TCP_KEEPIDLE)");
+  }
+
+  if ((nbytes = recv(fd, buf, MAX_SIZE - 1, 0)) == -1) {
+    perror("recv");
+    return 3;
+  }
+
+  buf[nbytes] = '\0';
+
+  printf("%s", buf);
+
+  while (is_true) {
+    is_true = some_func(fd);
+  }
+
+  close(fd);
+
   return 0;
 }
