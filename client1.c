84652b571520bde0ac3d3da412b1d6e0975cbb48
Amuzing <10791383+Amuzing@users.noreply.github.com>
2018-11-16 16:52:45 +0300

    Update client1.c

--- client1.c
+++ client1.c
@@ -16,7 +16,19 @@
 #define PORT "5678"
 #define MAX_SIZE 256
 
-bool some_func(int fd);
+#define CMD_NAME "0 "
+#define CMD_SAVE "1 "
+#define CMD_REM "2 "
+#define CMD_LIST "3 "
+
+int send_name(int fd, char* buf);
+
+bool handle_command(int fd, char* buf);
+
+int do_string(int fd, char* cmd, char* buf);
+int read_strings(int fd, char* buf);
+
+int send_msg(int fd, char* msg);
 
 int main(int argc, char* argv[]) {
   int fd;
@@ -25,7 +37,7 @@ int main(int argc, char* argv[]) {
   int yes = 1;
   int idle_time = 60;
   int rv;
-  bool is_true = true;
+  bool is_true = false;
   char* address = NULL;
 
   char buf[MAX_SIZE];
@@ -78,7 +90,8 @@ int main(int argc, char* argv[]) {
   if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &yes, sizeof(yes)) == -1) {
     perror("setsockopt(SO_KEEPALIVE)");
   }
-  if (setsockopt(fd, IPPROTO_IP, TCP_KEEPIDLE, &idle_time, sizeof(idle_time)) == -1) {
+  if (setsockopt(fd, SOL_TCP, TCP_KEEPIDLE, &idle_time, sizeof(idle_time)) ==
+      -1) {
     perror("setsockopt(TCP_KEEPIDLE)");
   }
 
@@ -92,10 +105,54 @@ int main(int argc, char* argv[]) {
   printf("%s", buf);
 
   while (is_true) {
-    is_true = some_func(fd);
+    is_true = handle_command(fd);
   }
 
   close(fd);
 
   return 0;
 }
+
+int send_msg(int fd, char* buf) {
+  if (send(fd, buf, strlen(buf) + 1, 0) == -1) {
+    perror("failed to send");
+    return 1;
+  }
+  return 0;
+}
+
+int do_string(int fd, char* cmd, char* buf) {
+  char temp[MAX_SIZE + sizeof(cmd)];
+  strcpy(temp, cmd);
+  strcat(temp, buf);
+  return send_msg(fd, temp);
+}
+
+int read_strings(int fd, char* buf) {
+  int nbytes;
+  bool is_true = true;
+  do_string(fd, CMD_LIST, buf);
+  while (is_true) {
+    if ((nbytes = recv(fd, buf, sizeof(buf), 0)) <= 0) {
+      if (nbytes == 0) {
+        printf("connection on socket %d was closed\n", i);
+        return 1;
+      } else {
+        perror("failed to recv");
+        return 2;
+      }
+    } else {
+      if(strlen(buf) != strlen("$") || strcmp(buf, "$")) {
+        printf("%s\n", buf);
+      } else {
+        is_true = false;
+      }
+    }
+  }
+  return 0;
+}
+
+bool handle_command(int fd, char* buf) {
+  
+}
+
