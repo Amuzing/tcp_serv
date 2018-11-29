3df8bdfb7febbd053d7870e4ae7635a85c21e5ba
Amuzing <10791383+Amuzing@users.noreply.github.com>
2018-11-29 17:34:15 +0300

    Slightly changed set_sock_opt func

--- include/tcp_comm_conn.c
+++ include/tcp_comm_conn.c
@@ -13,13 +13,14 @@ int get_addr_info(const char* address, const char* port, struct addrinfo** ai)
 }
 
 
-int set_sock_keepalive_opt(int fd, const int *keep_idle, const int *probes_cnt, const int *probes_intvl) 
+int set_sock_keepalive_opt(int fd, const int *keep_idle, const int *probes_intvl, const int *probes_cnt) 
 {
   int yes = 1;
   if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &yes, sizeof(true)) == -1) {
     perror("setsockopt(SO_KEEPALIVE)");
     return -1;
   } 
+  
   if (keep_idle != NULL) {
     if (setsockopt(fd, SOL_TCP, TCP_KEEPIDLE, keep_idle, sizeof(*keep_idle)) == -1) {
       perror("setsockopt(TCP_KEEPIDLE)");
@@ -27,15 +28,15 @@ int set_sock_keepalive_opt(int fd, const int *keep_idle, const int *probes_cnt, 
     }
   }
 
-  if (probes_cnt != NULL) {
-    if (setsockopt(fd, SOL_TCP, TCP_KEEPCNT, probes_cnt, sizeof(*probes_cnt)) == -1) {
+  if (probes_intvl != NULL) {
+    if (setsockopt(fd, SOL_TCP, TCP_KEEPINTVL, probes_intvl, sizeof(*probes_intvl)) == -1) {
       perror("setsockopt(TCP_KEEPIDLE)");
       return -3;
     }
   }
 
-  if (probes_intvl != NULL) {
-    if (setsockopt(fd, SOL_TCP, TCP_KEEPINTVL, probes_intvl, sizeof(*probes_intvl)) == -1) {
+  if (probes_cnt != NULL) {
+    if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPCNT, probes_cnt, sizeof(*probes_cnt)) == -1) {
       perror("setsockopt(TCP_KEEPIDLE)");
       return -4;
     }
