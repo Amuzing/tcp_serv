daf6e5ea6718f94637fba86cd120e86787b64ca3
Amuzing <vamuzinski@gmail.com>
2018-12-04 17:47:49 +0300

    Added byte_size header to msgs in test

--- tests/tcp_server_test.py
+++ tests/tcp_server_test.py
@@ -3,11 +3,12 @@
 
 
 import socket
+# import sys
 from random import choices
 from string import ascii_uppercase, digits
 from time import sleep
 
-
+UINT32_T_SIZE = 4
 MSG_SIZE = 65535
 RAND_STR_LEN = 10
 CMD_NAME = b'0'
@@ -15,6 +16,27 @@ CMD_ADD  = b'1'
 CMD_REM  = b'2'
 CMD_LIST = b'3'
 
+def serialize_int(x):
+    x = socket.htonl(x)
+    return x.to_bytes(UINT32_T_SIZE, byteorder = 'little')
+
+def deserialize_int(l):
+    x = int.from_bytes(l, byteorder = 'little')
+    return socket.ntohl(x)
+
+def send_str(s, msg):
+    total_size = UINT32_T_SIZE + len(msg)
+    new_msg = serialize_int(total_size) + msg
+    s.send(new_msg)
+    return
+
+def recv_str(s):
+    temp_data = s.recv(MSG_SIZE)
+    total_size = deserialize_int(temp_data[:4])
+    # print(total_size, temp_data)
+    assert(total_size == len(temp_data))
+    return temp_data[4:]
+    
 def get_rand_str(n):
     return ''.join(choices(ascii_uppercase + digits, k = n))
 
@@ -24,54 +46,59 @@ def get_conn_sock(_ip, _port):
     return temp_fd
 
 def get_data_from_server(_socket):
-    _socket.send(CMD_LIST)
-    return str(_socket.recv(MSG_SIZE)).replace(r"\n", "\n").split("\n")
+    send_str(_socket, CMD_LIST)
+    # _socket.send(CMD_LIST)
+    return str(recv_str(_socket)).replace(r"\n", "\n").split("\n")
+    # return str(_socket.recv(MSG_SIZE)).replace(r"\n", "\n").split("\n")
 
 
 ip_address = "127.0.0.1"
 ip_port = 5678
 serv_name = "tcp_server_test"
 
-s = get_conn_sock(ip_address, ip_port)
+try:
+    s = get_conn_sock(ip_address, ip_port)
 
 
+    recv_str(s)
+    # s.recv(MSG_SIZE)
 
-s.recv(MSG_SIZE)
+    send_str(s, CMD_NAME + bytes(serv_name, "utf-8"))
+    # s.send(CMD_NAME + bytes(serv_name, "utf-8"))
 
-s.send(CMD_NAME + bytes(serv_name, "utf-8"))
-
-my_test_strings = []
-for i in range(12):
-    my_test_strings.append(str(get_rand_str(RAND_STR_LEN)))
-
-
-for item in my_test_strings:
-    s.send(CMD_ADD + bytes(item, "utf-8"))
-    sleep(0.125)
+    my_test_strings = []
+    for i in range(12):
+        my_test_strings.append(str(get_rand_str(RAND_STR_LEN)))
 
 
-data = get_data_from_server(s)
-
-for item in my_test_strings:
-    temp = item + ", by " + str(serv_name)
-    assert(temp in data), temp + " is not in data"
-else:
-    print("Everything is ok, all strings arrived")
-
-    
-for item in my_test_strings:
-    s.send(CMD_REM + bytes(item, "utf-8"))
-    sleep(0.125)
+    for item in my_test_strings:
+        send_str(s, CMD_ADD + bytes(item, "utf-8"))
+        # s.send(CMD_ADD + bytes(item, "utf-8"))
+        # sleep(0.125)
 
 
-data = get_data_from_server(s)
+    data = get_data_from_server(s)
 
-for item in my_test_strings:
-    temp = item + ", by " + str(serv_name)
-    assert(temp not in data), temp + " is in data" 
-else:
-    print("Everything is ok, all strings were deleted")
+    for item in my_test_strings:
+        temp = item + ", by " + str(serv_name)
+        assert(temp in data), temp + " is not in data"
+    else:
+        print("Everything is ok, all strings arrived")
 
 
-s.close()
+    for item in my_test_strings:
+        send_str(s, CMD_REM + bytes(item, "utf-8"))
+        # s.send(CMD_REM + bytes(item, "utf-8"))
+        sleep(0.125)
 
+
+    data = get_data_from_server(s)
+
+    for item in my_test_strings:
+        temp = item + ", by " + str(serv_name)
+        assert(temp not in data), temp + " is in data" 
+    else:
+        print("Everything is ok, all strings were deleted")
+
+finally:
+    s.close()
